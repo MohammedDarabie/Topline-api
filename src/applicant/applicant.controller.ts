8ca8999853b539633e889550aa534ccc302b8256
MohammedDarabie <darabiemohammad@gmail.com>
2023-12-20 00:48:28 +0300

    Hello

--- src/applicant/applicant.controller.ts
+++ src/applicant/applicant.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApplicantService } from './applicant.service';
 import { createApplicantDto } from 'src/dto/create-applicant.dto';
 import { ApiResponseDto } from 'src/dto/api-response.dto';
+import { AuthMiddleware } from 'src/auth/auth.middleware';
 
 @Controller('api/applicant')
 export class ApplicantController {
   constructor(private readonly service: ApplicantService) {}
 
   @Get()
+  @UseGuards(AuthMiddleware)
   public async getUsers() {
     return this.service.getUsers();
   }
